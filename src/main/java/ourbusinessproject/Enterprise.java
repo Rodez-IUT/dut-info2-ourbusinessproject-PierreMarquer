d41d00a83db950cbd26271c24f17881679b6ecd8
PierreMarquer <pierre.marquer@iut-rodez.fr>
2020-02-03 13:45:37 +0100

    fix 1.2.5 classe Enterprise

---
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -0,0 +1,50 @@
+package ourbusinessproject;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class Enterprise {
+
+    @NotEmpty
+    private String name;
+    @NotEmpty @Length(min = 10, message = "The field must be at least 10 characters")
+    private String description;
+    @NotEmpty
+    private String contactName;
+    @NotEmpty @Email
+    private String contactEmail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+}
